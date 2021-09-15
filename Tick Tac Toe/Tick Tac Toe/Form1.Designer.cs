5149f3d5b869cdd0a17bc3313410ff91f9a8c6a2
Thomas Arensman <tharensman@gmail.com>
2021-09-15 14:36:10 +0200

    first commit for tick tac toe

---
+++ Tick Tac Toe/Tick Tac Toe/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace Tick_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Square1 = new System.Windows.Forms.Button();
+            this.Square3 = new System.Windows.Forms.Button();
+            this.Square5 = new System.Windows.Forms.Button();
+            this.Square9 = new System.Windows.Forms.Button();
+            this.Square7 = new System.Windows.Forms.Button();
+            this.Square2 = new System.Windows.Forms.Button();
+            this.Square4 = new System.Windows.Forms.Button();
+            this.Square6 = new System.Windows.Forms.Button();
+            this.Square8 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Square1
+            // 
+            this.Square1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Square1.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Square1.Location = new System.Drawing.Point(50, 50);
+            this.Square1.Name = "Square1";
+            this.Square1.Size = new System.Drawing.Size(100, 100);
+            this.Square1.TabIndex = 0;
+            this.Square1.UseVisualStyleBackColor = false;
+            this.Square1.Click += new System.EventHandler(this.Square1_Click);
+            // 
+            // Square3
+            // 
+            this.Square3.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Square3.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Square3.Location = new System.Drawing.Point(250, 50);
+            this.Square3.Name = "Square3";
+            this.Square3.Size = new System.Drawing.Size(100, 100);
+            this.Square3.TabIndex = 1;
+            this.Square3.UseVisualStyleBackColor = false;
+            this.Square3.Click += new System.EventHandler(this.Square3_Click);
+            // 
+            // Square5
+            // 
+            this.Square5.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Square5.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Square5.Location = new System.Drawing.Point(150, 150);
+            this.Square5.Name = "Square5";
+            this.Square5.Size = new System.Drawing.Size(100, 100);
+            this.Square5.TabIndex = 2;
+            this.Square5.UseVisualStyleBackColor = false;
+            this.Square5.Click += new System.EventHandler(this.Square5_Click);
+            // 
+            // Square9
+            // 
+            this.Square9.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Square9.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Square9.Location = new System.Drawing.Point(250, 250);
+            this.Square9.Name = "Square9";
+            this.Square9.Size = new System.Drawing.Size(100, 100);
+            this.Square9.TabIndex = 4;
+            this.Square9.UseVisualStyleBackColor = false;
+            this.Square9.Click += new System.EventHandler(this.Square9_Click);
+            // 
+            // Square7
+            // 
+            this.Square7.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Square7.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Square7.Location = new System.Drawing.Point(50, 250);
+            this.Square7.Name = "Square7";
+            this.Square7.Size = new System.Drawing.Size(100, 100);
+            this.Square7.TabIndex = 3;
+            this.Square7.UseVisualStyleBackColor = false;
+            this.Square7.Click += new System.EventHandler(this.Square7_Click);
+            // 
+            // Square2
+            // 
+            this.Square2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Square2.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Square2.Location = new System.Drawing.Point(150, 50);
+            this.Square2.Name = "Square2";
+            this.Square2.Size = new System.Drawing.Size(100, 100);
+            this.Square2.TabIndex = 5;
+            this.Square2.UseVisualStyleBackColor = false;
+            this.Square2.Click += new System.EventHandler(this.Square2_Click);
+            // 
+            // Square4
+            // 
+            this.Square4.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Square4.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Square4.Location = new System.Drawing.Point(50, 150);
+            this.Square4.Name = "Square4";
+            this.Square4.Size = new System.Drawing.Size(100, 100);
+            this.Square4.TabIndex = 6;
+            this.Square4.UseVisualStyleBackColor = false;
+            this.Square4.Click += new System.EventHandler(this.Square4_Click);
+            // 
+            // Square6
+            // 
+            this.Square6.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Square6.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Square6.Location = new System.Drawing.Point(250, 150);
+            this.Square6.Name = "Square6";
+            this.Square6.Size = new System.Drawing.Size(100, 100);
+            this.Square6.TabIndex = 7;
+            this.Square6.UseVisualStyleBackColor = false;
+            this.Square6.Click += new System.EventHandler(this.Square6_Click);
+            // 
+            // Square8
+            // 
+            this.Square8.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Square8.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Square8.Location = new System.Drawing.Point(150, 250);
+            this.Square8.Name = "Square8";
+            this.Square8.Size = new System.Drawing.Size(100, 100);
+            this.Square8.TabIndex = 8;
+            this.Square8.UseVisualStyleBackColor = false;
+            this.Square8.Click += new System.EventHandler(this.Square8_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(50, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(283, 37);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Display team that won";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 403);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Square8);
+            this.Controls.Add(this.Square6);
+            this.Controls.Add(this.Square4);
+            this.Controls.Add(this.Square2);
+            this.Controls.Add(this.Square9);
+            this.Controls.Add(this.Square7);
+            this.Controls.Add(this.Square5);
+            this.Controls.Add(this.Square3);
+            this.Controls.Add(this.Square1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Square1;
+        private System.Windows.Forms.Button Square3;
+        private System.Windows.Forms.Button Square5;
+        private System.Windows.Forms.Button Square9;
+        private System.Windows.Forms.Button Square7;
+        private System.Windows.Forms.Button Square2;
+        private System.Windows.Forms.Button Square4;
+        private System.Windows.Forms.Button Square6;
+        private System.Windows.Forms.Button Square8;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Tick Tac Toe/Tick Tac Toe/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Tick_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        public string move = "X";
+
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "";
+        }
+
+        // If the last move placed was an X change the current move to O and vice versa
+        private void TeamMove(String lastmove)
+        {
+            if (lastmove == "X")
+            {
+                move = "O";
+            }
+            else
+            {
+                move = "X";
+            }
+        }
+
+        private void checkWins ()
+        {
+            // Create a list of button arrays. Each button array contains 3 buttons that are required to make a winning row, col, or diagnal.
+            // For example btn Square1 btn Square2 btn Square3 are the first 3 buttons on the top row. If all of these are checked by one team it means that team has won 3 in a row
+            List<Button[]> allPosibleWins = new List<Button[]>();
+            allPosibleWins.Add(new Button[] { Square1, Square2, Square3 });
+            allPosibleWins.Add(new Button[] { Square4, Square5, Square6 });
+            allPosibleWins.Add(new Button[] { Square7, Square8, Square9 });
+            allPosibleWins.Add(new Button[] { Square1, Square4, Square7 });
+            allPosibleWins.Add(new Button[] { Square2, Square5, Square8 });
+            allPosibleWins.Add(new Button[] { Square3, Square6, Square9 });
+            allPosibleWins.Add(new Button[] { Square1, Square5, Square9 });
+            allPosibleWins.Add(new Button[] { Square3, Square5, Square7 });
+
+            // Loop over all the button arrays in the list at once
+            foreach (Button[] buttons in allPosibleWins)
+            {
+                // Loop through each button in the array and check if the text is equal to the team that just did a move
+                // If a button on a row, col or diagnal is equal to the playing team increase int checks by one
+                int checks = 0;
+                for (int i = 0; i < buttons.Length; i++) {
+                    if (buttons[i].Text == move)
+                    {
+                        checks += 1;
+                    }
+                }
+
+                // If checks equals 3 that means that there is a full row, col or diagnal checked by one team meaning: Tick tac toe, three in a row!
+                // Now that a team has won, display the winning team and disable all buttons to stop the players from clicking it
+                if (checks == 3)
+                {
+                    label1.Text = $"Team {move} won!";
+                    Square1.Enabled = false;
+                    Square2.Enabled = false;
+                    Square3.Enabled = false;
+                    Square4.Enabled = false;
+                    Square5.Enabled = false;
+                    Square6.Enabled = false;
+                    Square7.Enabled = false;
+                    Square8.Enabled = false;
+                    Square9.Enabled = false;
+                }
+            }
+        }
+
+        /*
+         * Down here I detect all button presses
+         * If the buttons text is empty you are able to click it
+         * After the click I call the method checkWins to check if it was a winning move
+         * At the end the method TeamMove changes the team from O to X or X to O
+         */
+
+        private void Square1_Click(object sender, EventArgs e)
+        {
+            if (Square1.Text == "")
+            {
+                Square1.Text = move;
+                checkWins();
+                TeamMove(move);
+            }
+        }
+
+        private void Square2_Click(object sender, EventArgs e)
+        {
+            if (Square2.Text == "")
+            {
+                Square2.Text = move;
+                checkWins();
+                TeamMove(move);
+            }
+        }
+
+        private void Square3_Click(object sender, EventArgs e)
+        {
+            if (Square3.Text == "")
+            {
+                Square3.Text = move;
+                checkWins();
+                TeamMove(move);
+            }
+        }
+
+        private void Square4_Click(object sender, EventArgs e)
+        {
+            if (Square4.Text == "")
+            {
+                Square4.Text = move;
+                checkWins();
+                TeamMove(move);
+            }
+        }
+
+        private void Square5_Click(object sender, EventArgs e)
+        {
+            if (Square5.Text == "")
+            {
+                Square5.Text = move;
+                checkWins();
+                TeamMove(move);
+            }
+        }
+
+        private void Square6_Click(object sender, EventArgs e)
+        {
+            if (Square6.Text == "")
+            {
+                Square6.Text = move;
+                checkWins();
+                TeamMove(move);
+            }
+        }
+
+        private void Square7_Click(object sender, EventArgs e)
+        {
+            if (Square7.Text == "")
+            {
+                Square7.Text = move;
+                checkWins();
+                TeamMove(move);
+            }
+        }
+
+        private void Square8_Click(object sender, EventArgs e)
+        {
+            if (Square8.Text == "")
+            {
+                Square8.Text = move;
+                checkWins();
+                TeamMove(move);
+            }
+        }
+
+        private void Square9_Click(object sender, EventArgs e)
+        {
+            if (Square9.Text == "")
+            {
+                Square9.Text = move;
+                checkWins();
+                TeamMove(move);
+            }
+        }
+    }
+}
