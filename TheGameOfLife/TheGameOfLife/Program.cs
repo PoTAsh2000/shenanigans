8df4e5f442f5d24c97e27da2671026cb789cd1d8
PoTAsh <tharensman@gmail.com>
2021-10-18 15:40:04 +0200

    The game of life program

---
+++ TheGameOfLife/TheGameOfLife/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Threading;
+
+namespace TheGameOfLife
+{
+    class Program
+    {
+        static Random random = new Random();
+
+        const int cols = 100;
+        const int rows = 30;
+
+        static void Main(string[] args)
+        {
+            // make cursor invisable
+            Console.CursorVisible = false;
+
+            // init the starting grid
+            string[,] grid = new string[cols, rows];
+
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    // generate a random number per x,y coord if the number 1 make the x,y coord alive, else the xy coord is dead
+                    int rnd = random.Next(11);
+                    if (rnd == 1) grid[x, y] = "█";
+                    else grid[x, y] = " ";
+                }
+            }
+
+            int genCount = 1;
+
+            while (true)
+            {
+                // every 50 gens create some new live cells on random location to keep the sim going
+                if (genCount % 5 == 0)
+                {
+                    for (int x = 0; x < cols; x++)
+                    {
+                        for (int y = 0; y < rows; y++)
+                        {
+                            int chance = random.Next(301);
+                            if (chance == 1) grid[x, y] = "█";
+                        }
+                    }
+                }
+
+                // init the next gen
+                string[,] nextGen = new string[cols, rows];
+
+                for (int x = 0; x < cols; x++)
+                {
+                    for (int y = 0; y < rows; y++)
+                    {
+                        // count the surounding tiles
+                        int liveNeihbors = countLiveNeihborTiles(grid, x, y);
+                        string state = grid[x, y];
+
+                        if (state == "█" && liveNeihbors == 2 || liveNeihbors == 3)
+                        {
+                            nextGen[x, y] = "█";
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        else if (state == " " && liveNeihbors == 3)
+                        {
+                            nextGen[x, y] = "█";
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        else
+                        {
+                            nextGen[x, y] = " ";
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        
+                        // draw the current grid/ gen
+                        Console.SetCursorPosition(x, y);
+                        Console.Write(grid[x, y]);
+                    }
+                }
+
+                // set the grid to the next gen
+                grid = nextGen;
+                // increase gen count per gen
+                genCount++;
+                Thread.Sleep(50);
+            }
+        }
+
+        static int countLiveNeihborTiles(string[,] grid, int x, int y)
+        {
+            int sum = 0;
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    int c = (x + i + cols) % cols;
+                    int r = (y + j + rows) % rows;
+                    if (grid[c, r] == "█") sum++;
+                }
+            }
+
+            if (grid[x, y] == "█") sum -= 1;
+            return sum;
+        }
+    }
+}
---
+++ TwoEggProblem/TwoEggProblem/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace TwoEggProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomFloor = random.Next(1, 100);
+
+            Console.WriteLine("Max floor: " + randomFloor);
+
+            int floor = FirstEgg(randomFloor, 0, 14, 0)[0];
+            int increase = FirstEgg(randomFloor, 0, 14, 0)[1];
+            int tries = FirstEgg(randomFloor, 0, 14, 0)[2];
+
+            Console.WriteLine("First egg broke at floor: " + floor);
+            Console.WriteLine("Tries: " + tries);
+
+            int startFloor = floor - increase;
+            Console.WriteLine("second egg start: " + startFloor);
+            Console.WriteLine("increase"+ increase);
+
+            for (int i = 0; i < increase; i++)
+            {
+                Console.WriteLine(1);
+                tries++;
+                if (i == randomFloor)
+                {
+                    Console.WriteLine("Seconds egg broke at:" + i);
+                    Console.WriteLine("At try: " + tries);
+                }
+
+            }
+        }
+
+        static int[] FirstEgg(int randomFloor, int floor, int increase, int tries)
+        {
+            floor += increase;
+
+            if (floor >= randomFloor) return new int[] { floor, increase, tries + 1 };
+
+            return FirstEgg(randomFloor, floor, increase - 1, tries + 1);
+        }
+
+    }
+}
